9c69748ab059179474204524c9acd365ac6c642a
dev-charles15531 <kingcharles15531@gmail.com>
2022-05-12 20:40:51 +0100

    All functionalities implemented

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 50 Days Of Vue JS
-## Day 2
+## Day 3
 
-This contains the complete day 1 project(Testimonial Slider).
-- Clone the repo and switch to the day 1 branch. 
+This contains the complete day 3 project(The Countdown).
+- Clone the repo and switch to the day 3 branch. 
 - On your terminal, cd into the projects directory and run the command: ```npm install```
 - After installing all packages, run the command ```npm run dev```
 
--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,35 @@
 <template>
     <div class="w-full">
-        <nav class="w-full px-6 py-3 bg-gradient-to-r from-cyan-500 to-blue-500 text-xl font-bold text-red-50">
-            All Testimonials
+        <nav class="w-full px-6 py-3 bg-gradient-to-r from-lime-600 to-green-500 text-xl font-bold text-red-50">
+            Countdown
         </nav>
         <div class="w-full flex justify-center mt-12">
-            <div v-if="testimonies.length" class="w-full md:w-3/5 bg-gradient-to-r from-cyan-200 to-blue-200 p-4 border rounded">
-                <div class="w-full bg-gradient-to-r from-cyan-100 to-blue-100 flex space-x-5 justify-evenly p-2">
-                    <div class="text-sm md:text-base">
-                        <img :src="testimonies[index].img" alt="image" class="h-24 w-24 md:h-32 md:w-32 rounded-full border border-pink-100">
-                        <div class="text-center">
-                            <span class="font-bold">{{testimonies[index].name}}</span>
-                        </div>
-                        <div class="text-center">
-                            <span class="font-semibold font-mono">{{testimonies[index].occupation}}</span>
-                        </div>
-                    </div>
-
-                    <div class="mt-4 md:mt-6 flex-1">
+            <div class="w-full md:w-3/5 bg-gradient-to-r from-pink-300 to-purple-300 p-4 border rounded">
+                <div class="flex justify-evenly">
+                    <div>
+                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{days}}</Span></div>
                         <div>
-                            <div class="flex items-center mb-2">
-                                <svg v-for="n in 5" class="w-5 h-5" :class="[n<=testimonies[index].rating? ' text-yellow-400':'text-gray-300 dark:text-gray-500']" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                            </div>
+                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Days</h2>
                         </div>
+                    </div>
+                    <div>
+                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{hours}}</Span></div>
                         <div>
-                            <q class="break-normal text-gray-700 text-xl font-sans italic hover:not-italic font-semibold">{{testimonies[index].testimony}}</q>
+                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Hours</h2>
                         </div>
                     </div>
-                </div>
-
-                <div class="mt-4 flex justify-evenly">
-                    <div><button @click="slidePrev" class="text-xl text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500 px-3">&LeftArrow;</button></div>
-                    <div class="inline-flex space-x-2">
-                        <button class="h-3 w-3 rounded-full" v-for="indicator in 5" @click="slideTo(indicator-1)" :class="[(indicator-1)==index? 'bg-gradient-to-r from-cyan-800 to-blue-800':'bg-gradient-to-r from-cyan-500 to-blue-500']">
-                        </button>
+                    <div>
+                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{minutes}}</Span></div>
+                        <div>
+                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Minutes</h2>
+                        </div>
                     </div>
-                    <div><button @click="slideNext" class="text-xl text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500 px-3">&rightarrow;</button></div>
+                    <div>
+                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{seconds}}</Span></div>
+                        <div>
+                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Seconds</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -42,104 +37,67 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                index: 0,
-                /**
-                 * Static testimonies data
-                 * 
-                 * @var array
-                 */
-                testimonies: [
-                    {
-                        name: "Michelle Takers", 
-                        img: "src/assets/imgs/img1.jpg", 
-                        occupation: "Web Developer", 
-                        rating: 3, 
-                        testimony: "Take my word for it gusy vue js is worth it. Check the community, projects and stackoverflow, you'll understand what i mean"
-                    },
-                    {
-                        name: "Charles Darwin", 
-                        img: "src/assets/imgs/img2.jpg", 
-                        occupation: "Software Engineer", 
-                        rating: 3, 
-                        testimony: "I had a pretty good time using vue js"
-                    },
-                    {
-                        name: "Lennon McPaul ", 
-                        img: "src/assets/imgs/img3.jpg", 
-                        occupation: "Web Developer", 
-                        rating: 4, 
-                        testimony: "I had to literally rewrite my company's website. Initially, it was jquery, an now vue. I love you Evan"
-                    },
-                    {
-                        name: "Drew Peters", 
-                        img: "src/assets/imgs/img4.jpg", 
-                        occupation: "CTO Tech Lead", 
-                        rating: 5, 
-                        testimony: "The mind blowing thing about vue js is its steep learning curve. Believe me, if you're a good js developer, you can master vue js in just 50 days."
-                    },
-                    {
-                        name: "Peter Parker", 
-                        img: "src/assets/imgs/img5.jpg", 
-                        occupation: "CEO Peakersoft", 
-                        rating: 5, 
-                        testimony: "I had a great time developing in vue js. It is fast and very scalable."
-                    },
-                ]
-            }
-        },
-        mounted () {
-            this.slide()
-        },
-        methods: {
-            /**
-             * This method loops the index from 0 - 5 at an interval of 7secs causing a sliding effect of the testimonials
-             */
-            slide() {
-                setInterval(() => {
-                    if(this.index == 5){
-                        this.index = 0
-                    }
-                    else {
-                        this.index++
-                    } 
-                }, 7000);
-            },
-
-            /**
-             * This method increments the index by one or resets to 0 if index equals 5
-             */
-            slideNext() {
-                if(this.index == 5) {
-                    this.index = 0
-                }
-                else {
-                    this.index++
-                } 
-            },
-
-            /**
-             * This method decrements the index by one or resets to 5 if index equals 0
-             */
-            slidePrev() {
-                if(this.index == 0) {
-                    this.index = 5
-                }
-                else {
-                    this.index--
-                } 
-            },
-
-            /**
-             * This method sets the index to a particular argument value
-             * 
-             * @param string index The value to set index to 
-             */
-            slideTo(index) {
-                this.index = index
-            },
+export default {
+    data() {
+        return {
+            dateFrom: new Date("Jun 28, 2022 10:00:00").getTime(),
+            interval: "",
+            funcInterval: "",
+            days: "",
+            hours: "",
+            minutes: "",
+            seconds: "",
+            elapseMssg: ""
         }
-    }
+    },
+    methods: {
+        /**
+         * Method to get the number of days to countdown
+         */
+        getDays() {
+            return Math.floor(this.interval / (1000 * 60 * 60 * 24))
+        },
+        /**
+         * Method to get the number of hours to countdown
+         */
+        getHours() {
+            return Math.floor((this.interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        },
+        /**
+         * Method to get the number of minutes to countdown
+         */
+        getMinutes() {
+            return Math.floor((this.interval % (1000 * 60 * 60)) / (1000 * 60))
+        },
+        /**
+         * Method to get the number of seconds to countdown
+         */
+        getSeconds() {
+            return Math.floor((this.interval % (1000 * 60)) / (1000))
+        },
+        startCountdown() {
+            this.funcInterval = setInterval(() => {
+                let dateTo = new Date().getTime()
+                this.interval = this.dateFrom - dateTo
+
+                this.days = this.getDays()
+                this.hours = this.getHours()
+                this.minutes = this.getMinutes()
+                this.seconds = this.getSeconds()
+
+                if(this.interval < 0) {
+                    clearInterval(this.funcInterval)
+                    this.elapseMssg = "Elap"
+                }
+            }, 1000);
+        }
+    },
+    mounted () {
+        this.startCountdown();
+    },
+}
 </script>
+
+<style lang="scss" scoped>
+
+</style>
