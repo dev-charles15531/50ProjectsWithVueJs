beec89bb0f4e825da6d2e72b4fb7e30726b2567d
dev-charles15531 <kingcharles15531@gmail.com>
2022-05-12 21:25:55 +0100

    FInished the countdown styling

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,42 @@
 <template>
     <div class="w-full">
-        <nav class="w-full px-6 py-3 bg-gradient-to-r from-lime-600 to-green-500 text-xl font-bold text-red-50">
+        <nav class="w-full px-6 py-3 bg-gradient-to-r from-rose-400 to-fuchsia-400 text-xl font-bold text-red-50">
             Countdown
         </nav>
-        <div class="w-full flex justify-center mt-12">
-            <div class="w-full md:w-3/5 bg-gradient-to-r from-pink-300 to-purple-300 p-4 border rounded">
+        <div class="w-full flex justify-center mt-24 px-1 md:px-2">
+            <div class="w-full md:w-3/5 bg-gradient-to-r from-pink-300 to-purple-300 px-1 py-2 md:p-4 border rounded">
+                <div class="text-2xl text-center text-gray-700 mb-4 font-black opacity-70">Countdown to the end of <b>50 Days Of Code</b> </div>
                 <div class="flex justify-evenly">
                     <div>
-                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{days}}</Span></div>
+                        <div class="h-20 w-20 md:h-24 md:w-24 border rounded opacity-50 bg-pink-100 flex justify-center items-center">
+                            <Span class="text-5xl font-black">{{days}}</Span>
+                        </div>
                         <div>
-                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Days</h2>
+                            <h2 class="text-xl md:text-2xl font-black text-center text-black text-opacity-50">Days</h2>
                         </div>
                     </div>
                     <div>
-                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{hours}}</Span></div>
+                        <div class="h-20 w-20 md:h-24 md:w-24 border rounded opacity-50 bg-pink-100 flex justify-center items-center">
+                            <Span class="text-5xl font-black">{{hours}}</Span>
+                        </div>
                         <div>
-                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Hours</h2>
+                            <h2 class="text-xl md:text-2xl font-black text-center text-black text-opacity-50">Hours</h2>
                         </div>
                     </div>
                     <div>
-                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{minutes}}</Span></div>
+                        <div class="h-20 w-20 md:h-24 md:w-24 border rounded opacity-50 bg-pink-100 flex justify-center items-center">
+                            <Span class="text-5xl font-black">{{minutes}}</Span>
+                        </div>
                         <div>
-                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Minutes</h2>
+                            <h2 class="text-xl md:text-2xl font-black text-center text-black text-opacity-50">Minutes</h2>
                         </div>
                     </div>
                     <div>
-                        <div class="h-24 w-24 border rounded opacity-50 bg-pink-100"> <Span>{{seconds}}</Span></div>
+                        <div class="h-20 w-20 md:h-24 md:w-24 border rounded opacity-50 bg-pink-100 flex justify-center items-center">
+                            <Span class="text-5xl font-black">{{seconds}}</Span>
+                        </div>
                         <div>
-                            <h2 class="text-2xl font-black text-center text-black text-opacity-50">Seconds</h2>
+                            <h2 class="text-xl md:text-2xl font-black text-center text-black text-opacity-50">Seconds</h2>
                         </div>
                     </div>
                 </div>
@@ -75,6 +84,9 @@ export default {
         getSeconds() {
             return Math.floor((this.interval % (1000 * 60)) / (1000))
         },
+        /**
+         * Method to start the countdown at interval of a second
+         */
         startCountdown() {
             this.funcInterval = setInterval(() => {
                 let dateTo = new Date().getTime()
