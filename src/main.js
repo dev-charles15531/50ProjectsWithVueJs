e2167880e19ed839277ed25236d66a9c4582ba57
dev-charles15531 <kingcharles15531@gmail.com>
2022-07-25 00:03:36 +0100

    Skeleton vue folder with design folder and project images folder from frontend-mentors

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createPinia } from 'pinia';
 import './assets/styles/styles.css';
 
-
-const pinia = createPinia()
-
 createApp(App)
-    .use(pinia)
     .mount('#app')
